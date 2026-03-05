ec80180e0e11cdc4bb9003f2bd955e1efd1fe08c
Marginfold <admin@marginfoldpress.com>
2026-03-05 22:53:07 +0000

    Update page.tsx

--- src/app/book/page.tsx
+++ src/app/book/page.tsx
@@ -33,7 +33,7 @@ function page({
           <p className="mt-3">Digital release available now.</p>
           <p className="mt-3">Hardback drops later this year.</p>
           
-          <Button onClick={() => setPopUpOpen(true)} className="btn primary mt-8">[ BUY THE DIGITAL EDITION £15 ]</Button>
+          <Button onClick={() => setPopUpOpen(true)} className="btn primary mt-8 text-lg px-8 py-3">[ BUY THE DIGITAL EDITION £15 ]</Button>
 
           <PopUp
             popUpOpen={popUpOpen}

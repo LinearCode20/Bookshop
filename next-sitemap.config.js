dbd3ad5a514d7eb28287ddb8d9261d1b02da55b2
Mukesh <coddermk@gmail.com>
2026-03-14 21:48:21 +0530

    sitemap file issue fixed

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,15 +1,20 @@
 /** @type {import('next-sitemap').IConfig} */
 const BASE_URL = process.env.BASE_URL ?? "https://www.letmegiveyouthegame.com";
-    
+
 module.exports = {
   siteUrl: BASE_URL,
   generateRobotsTxt: true,
   sitemapSize: 7000,
-  changefreq: 'daily',
+  changefreq: "daily",
   priority: 0.7,
+
   exclude: [
-    '/auth/*',
-    '/custom-action',
-    '/login',
+    "/auth/*",
+    "/custom-action",
+    "/login",
   ],
-}
+
+  additionalPaths: async (config) => [
+    await config.transform(config, "/book"),
+  ],
+};
